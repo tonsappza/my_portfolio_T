7f4a8224ed61c7c7732bee5022e57034ba2af924
tonsappza <119911353+tonsappza@users.noreply.github.com>
2023-03-24 16:50:00 +0700

    T

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { Card, Row, Form, Input, Button, Col, Image, Space } from "antd";
 import "../styles/bg-Contact.css"
 import QRcode from '../../src/assets/images/QRcodeMe.jpg'
+import { CopyToClipboard } from "react-copy-to-clipboard"
 
 const MYContact = () => {
-    const { TextArea } = Input;
     return (
         <>
             <div class="bg-contact">
@@ -28,16 +29,38 @@ const MYContact = () => {
                         <Col span={24}>
                             <h2 style={{ textAlign: 'center' }}>Contact me</h2>
                             <Form.Item label={'Email'}>
-                                <Input className="disEmail"
-                                    disabled
-                                    value={'tonpongsapak.02102541@gmail.com'}>
-                                </Input>
+                                <Row>
+                                    <Col xs={24} sm={24} xl={19} xxl={19}>
+                                        <Input className="disEmail"
+                                            disabled
+                                            value={'tonpongsapak.02102541@gmail.com'}>
+                                        </Input>
+                                    </Col>
+                                    <Col>
+                                        <Space className="Space-copy">
+                                            <CopyToClipboard text={'tonpongsapak.02102541@gmail.com'}>
+                                                <Button className="btn-copy">Copy Email</Button>
+                                            </CopyToClipboard>
+                                        </Space>
+                                    </Col>
+
+                                </Row>
                             </Form.Item >
                             <Form.Item label={'Phone'} >
-                                <Input className="disEmail"
-                                    disabled
-                                    value={'096-898-7514'}>
-                                </Input>
+                                <Row>
+                                    <Col xs={24} sm={24} xl={19} xxl={19}>
+                                        <Input className="disEmail"
+                                            disabled
+                                            value={'096-898-7514'}>
+                                        </Input>
+                                    </Col>
+                                    <Col >
+                                        <CopyToClipboard text={'0968987514'}
+                                        >
+                                            <Button className="btn-copy">Copy Phone</Button>
+                                        </CopyToClipboard>
+                                    </Col>
+                                </Row>
                             </Form.Item>
                             <h2 style={{ textAlign: 'center' }}>My Line QR code</h2>
                             <Space className="QRcodeSpace">
